c2b9ffe2c61e109af6e173ad96134e61029f5dd8
Hrisika <hjagdeep@asu.edu>
2025-10-09 00:05:39 -0700

    server js file should work now to encrypt thomas' secret key under the locations csv tileset I created

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ require('dotenv').config();
 
 const app = express();
 
-app.get('/mapbox-data', async (req, res) => {
+app.get('/mapbox-tileset', async (req, res) => {
   try {
-    const response = await axios.get('https://api.mapbox.com/datasets/v1/YOUR_USERNAME/YOUR_DATASET_ID/features', {
-      headers: {
-        Authorization: `Bearer ${process.env.MAPBOX_SECRET}`
+    const tilesetId = 'hjagdeep.cmgiy3sld25iq1omnu66a4ego-2jrng'; //this needs to be public? i think?
+
+    const response = await axios.get(`https://api.mapbox.com/v4/${tilesetId}.json`, {
+      params: {
+        access_token: process.env.MAPBOX_SECRET
       }
     });
+
     res.json(response.data);
   } catch (error) {
-    res.status(500).send('Error fetching Mapbox data');
+    console.error('Mapbox Tileset API error:', error.response?.data || error.message);
+    res.status(500).send('Error fetching tileset metadata');
   }
 });
 
